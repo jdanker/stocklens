05a463c7f954eb7acb6917c9f393285c4d59372a
Jahred Danker <jahrede@gmail.com>
2025-03-06 19:45:35 -0600

    redid structure in prep for python

--- cli/main.go
+++ cli/main.go
@@ -4,7 +4,7 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/jdanker/stocklens/cmd"
+import "github.com/jdanker/stocklens/cli/cmd"
 
 func main() {
 	cmd.Execute()
